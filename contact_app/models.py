ec6b4b3cd461f4fb45df2699c31d2e2a234712a7
jayednahian <jayednahian@yahoo.com>
2021-09-30 17:58:00 +0600

    update_id_field

--- contact_app/models.py
+++ contact_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class ContactList(models.Model):
+   id       = models.IntegerField(unique=True,primary_key=True)
    name     = models.CharField(max_length=100)
    numbers  = models.CharField(max_length=50,unique=True)
 
